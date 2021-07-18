d2c953f84e5419e8e4af1ed3de2cdd8c76330968
Jonatas Fontele <jonatasfontele@gmail.com>
2021-07-17 23:53:13 -0300

    More solution

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,3 +1,7 @@
+# 61 characters
+i=1
+while i<101:print("Fizz"*(i%3<1)+"Buzz"*(i%5<1)or i);i+=1
+
 # 59 characters
 # Bit Operator -~i = -(-(i+1)
 for i in range(100):print(i%3//2*'Fizz'+i%5//4*'Buzz'or-~i)
